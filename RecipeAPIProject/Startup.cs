7141c18699f2c732a0a2f78f13dcc2543ebb7a82
imsxz <lukelenhard@gmail.com>
2019-06-06 13:50:14 -0400

    initial

---
+++ RecipeAPIProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecipeAPIProject.Startup))]
+namespace RecipeAPIProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
