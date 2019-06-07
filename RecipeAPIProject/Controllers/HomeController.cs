413506d01bf17412d606701af342e434aeba597b
Andre Watts <andrewattsii@gmail.com>
2019-06-07 12:52:54 -0400

    sub2

--- RecipeAPIProject/Controllers/HomeController.cs
+++ RecipeAPIProject/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace RecipeAPIProject.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
