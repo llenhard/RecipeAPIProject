ed83d79b36d2bd362dad33ec135ec5607c4db501
imsxz <lukelenhard@gmail.com>
2019-06-09 18:48:23 -0400

    last stuff

--- RecipeAPIProject/Controllers/HomeController.cs
+++ RecipeAPIProject/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using Newtonsoft.Json.Linq;
 using RecipeAPIProject.Models;
 
@@ -10,23 +12,66 @@ namespace RecipeAPIProject.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private RecipeDbEntities ORM = new RecipeDbEntities();
+
+        public ActionResult Index(string type = null, string input = null)
         {
-           ViewBag.results = RecipeDAL.GetRecipes(null)["results"];
+           ViewBag.results = RecipeDAL.GetRecipes($"?{type}{input}")["results"];
            return View();
         }
 
-        public ActionResult About()
+        public ActionResult About(string item)
         {
-            
-            return View(RecipeDAL.GetRecipes(null)["results"]);
+            JObject parsed = JObject.Parse(item);
+            return View(parsed);
         }
 
         public ActionResult Contact()
         {
-            
+            List<JObject> favorites = new List<JObject>();
+            string userId = User.Identity.GetUserId();
 
-            return View();
+            foreach(var item in ORM.Favorites.Where(f => f.UserID == userId).ToList())//lol
+            {
+                JObject favorite = JObject.Parse(item.RecipeTitle);
+                favorites.Add(favorite);
+            }
+
+            return View(favorites);
+        }
+
+        public ActionResult SaveNewFavorite(string item)
+        {
+            string userId = User.Identity.GetUserId();
+
+            if(userId == null)
+            {
+                return RedirectToAction("../Account/Login");
+            }
+
+            Favorites newFav = new Favorites();
+            newFav.UserID = userId;
+            newFav.RecipeTitle = item;
+            ORM.Favorites.Add(newFav);
+            ORM.SaveChanges();
+            return RedirectToAction("../Home/Contact");
+        }
+
+        public ActionResult RemoveFavorite(string item)
+        {
+            string userId = User.Identity.GetUserId();
+
+            if (userId == null)
+            {
+                return RedirectToAction("../Account/Login");
+            }
+
+            Favorites favorite = ORM.Favorites.Where(f => f.UserID == userId && f.RecipeTitle == item).First();
+            ORM.Favorites.Attach(favorite);
+            ORM.Favorites.Remove(favorite);
+            ORM.SaveChanges();
+            
+            return RedirectToAction("../Home/Contact");
         }
     }
 }
